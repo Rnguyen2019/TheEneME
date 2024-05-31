e8a9616f1e7102385f2a28c288c1fd38f8969d90
Rnguyen2019 <rnguyen2019@student.pps.net>
2024-05-31 11:36:41 -0700

    Git Initial Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public Gameobject[] targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.Gameobject.Vector2.MoveTowards(transform.position,targets[0].transform.position, 10);
+    }
+}
